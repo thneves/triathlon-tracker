5bb6acb1b76ade7bbc82535b3d30912e5a7962fd
thneves <tsneves11@gmail.com>
2021-08-10 05:59:00 -0300

    Create route for api

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::TracksController < ApplicationController
+  before_action :set_track, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  
+  def set_track
+    @track = Track.find(params[:id])
+  end
+
 end
