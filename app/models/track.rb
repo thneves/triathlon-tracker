888313b2258eea31bc56177945d3a8bce3bce5ce
thneves <tsneves11@gmail.com>
2021-08-10 05:50:22 -0300

    Implement relationship betwen user and tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,10 @@
 class Track < ApplicationRecord
+  default_scope { order(created_at: :desc) }
   belongs_to :user
+
+  validates :sport, presence: true
+  validates :moving_time, presence: true
+  validates :distance, presence: true
+  validates :day, presence: true
+
 end
