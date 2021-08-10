172ae5e5cbf6b91e3f6199464ad633aa35708719
thneves <tsneves11@gmail.com>
2021-08-10 09:02:12 -0300

    Set authorized endpoint

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::TracksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_track, only: [:show, :edit, :update, :destroy]
 
   def index
+    @tracks = current_user.tracks.all
   end
 
   def show
+    if authorized?
+      respond_to do |format|
+        format.json { render :show }
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def create
@@ -20,6 +29,18 @@ class Api::V1::TracksController < ApplicationController
   
   def set_track
     @track = Track.find(params[:id])
+  end
+
+  def authorized?
+    @track.user == current_user
+  end
+
+  def handle_unauthorized
+    unless authorized?
+      respond_to do |format|
+        format.json { render :unauthorized, status: 401 }
+      end
+    end 
   end
 
 end
