ecef6ba2df9eb884b4744bde50d685598e108a81
thneves <tsneves11@gmail.com>
2021-08-10 02:32:47 -0300

    Implement Login page, style and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#home'
-  get 'pages/triathlon_tracker'
+
+  unauthenticated do
+    root 'pages#home'
+  end
+
+  authenticated :user do
+    root "pages#triathlon_tracker", as: :authenticated_root
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
