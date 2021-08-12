266bd0584b03f8eafa4fe3aecfba34bcd22ec154
thneves <tsneves11@gmail.com>
2021-08-12 19:24:01 -0300

    Update registration controller with login and logout methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
-class SessionsController > ApplicationController
+class SessionsController < ApplicationController
+  include CurrentUserConcern
   def create
     user = User.find_by(email: params["user"]["email"])
                .try(:authenticate, params["user"]["password"])
@@ -15,5 +16,23 @@ class SessionsController > ApplicationController
         status: 401
       }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
   end
 end
