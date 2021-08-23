faf5d8a487803e977815988eecf3e8fd46d7838f
thneves <tsneves11@gmail.com>
2021-08-23 16:00:22 -0300

    Update cors allow

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,12 +3,7 @@
 # cors gives the ability to white list certain domains
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: %i[get post put patch delete head], credentials: true # allows
-  end
-
-  allow do
     origins 'https://tri-tracker.herokuapp.com/' # for production
-    resource '*', headers: :any, methods: %i[get post put patch delete head], credentials: true # allows
+    resource '*', headers: :any, methods: %i[get post put patch delete head options], credentials: true # allows
   end
 end
