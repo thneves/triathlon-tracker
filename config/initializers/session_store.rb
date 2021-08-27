35a6d2b9e7c687a8a19110a4e87e07939d01b7c1
thneves <tsneves11@gmail.com>
2021-08-27 13:09:40 -0300

    Enable SSL to true

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,16 +2,7 @@
 
 # define how the cookies structure will be like
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, 
-    :key => '_authentication_app',
-    :domain => "tri-tracker.herokuapp.com",
-    :secure => :true,
-    :same_site => :none
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "tri-tracker.herokuapp.com"
 else
-  Rails.application.config.session_store :cookie_store, 
-    :key => '_authentication_app',
-    :domain => "tri-tracker.herokuapp.com",
-    :secure => :true,
-    :same_site => :none
-  
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
