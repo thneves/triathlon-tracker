b3c18e75dcc5f438e880984d81813567c339c0a0
thneves <tsneves11@gmail.com>
2021-08-27 18:21:26 -0300

    Change session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 # define how the cookies structure will be like
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "https://tri-tracker.herokuapp.com", expire_after: 60.minutes
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: :all, tld_length: 2
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
