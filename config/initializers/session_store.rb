6e879eb8b72cbbb7f76c1eb0d2bac460ef566787
thneves <tsneves11@gmail.com>
2021-08-27 18:37:38 -0300

    Update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 # define how the cookies structure will be like
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: :all, tld_length: 2
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "https://tri-tracker.herokuapp.com", same_site: 'none'
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
