dbbc8e8647031b44aa1bdb4dc21ef3a894f96c19
thneves <tsneves11@gmail.com>
2021-08-27 21:30:54 -0300

    Set api onlu to false

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,5 @@ module TriathlonTracker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.api_only = true
   end
 end
