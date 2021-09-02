bc5f31d4bfd6ef758da4b620e534a070043fd8b4
thneves <tsneves11@gmail.com>
2021-09-02 15:34:12 -0300

    Fix undefined local variable, set to false

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 # define how the cookies structure will be like
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "https://tri-tracker.herokuapp.com", sameSite: none
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "https://tri-tracker.herokuapp.com", sameSite: false
 else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
