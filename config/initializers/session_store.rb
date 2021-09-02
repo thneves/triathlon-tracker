752a00c452391735476c9f05e87c41c629130ca7
thneves <tsneves11@gmail.com>
2021-09-02 15:30:43 -0300

    Set sameSite to none

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,6 @@
 
 # define how the cookies structure will be like
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "https://tri-tracker.herokuapp.com", same_site: false
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "https://tri-tracker.herokuapp.com", sameSite: none
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
