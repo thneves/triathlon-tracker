ebe936c0ebe5f5da6f40f3ca95904faa12cebe26
thneves <tsneves11@gmail.com>
2021-09-02 15:31:33 -0300

    fix else statement

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,6 @@
 # define how the cookies structure will be like
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: "https://tri-tracker.herokuapp.com", sameSite: none
+else
   Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
